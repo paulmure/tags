ea89a4c042d263dcf5fe6e9e7de2f1bf7f56c314
Paul Mure <paulmure2000@gmail.com>
2023-05-18 21:52:56 -0700

    train set

--- fetch_dataset.sh
+++ fetch_dataset.sh
@@ -4,3 +4,5 @@ wget https://archive.org/download/nf_prize_dataset.tar/nf_prize_dataset.tar.gz
 tar -xvzf nf_prize_dataset.tar.gz
 mv download/ data/
 rm nf_prize_dataset.tar.gz
+
+tar -xvf data/training_set.tar -C data
