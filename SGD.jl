e943cd4e3ea0c5ff0c0ee93110af0c67faa9d892
Paul Mure <paulmure2000@gmail.com>
2023-05-20 00:48:45 -0700

    update plot

--- SGD.jl
+++ SGD.jl
@@ -33,11 +33,11 @@ function train(data::MatrixData{Float32}, nfeatures::Int, μ::Float32, learning_
     println(initial_loss)
     push!(history, initial_loss)
 
-    for _ in 1:100
+    for i in 1:100
         ReverseDiff.gradient!(results, backprop, inputs)
         L -= results[1] .* learning_rate
         R -= results[2] .* learning_rate
-        
+
         curr_loss = loss(L, R)
         last_loss = history[end]
 
@@ -45,15 +45,17 @@ function train(data::MatrixData{Float32}, nfeatures::Int, μ::Float32, learning_
 
         println(curr_loss)
 
-        if curr_loss > last_loss
+        if (i > 100) && (curr_loss > last_loss)
             break
         end
     end
 
-    plot(1:length(history), history)
+    plot(1:length(history), history, label="loss")
+    xlabel!("Iteration")
+    ylabel!("Loss")
 end
 
-data = load_netflix_data(8);
+data = load_netflix_data(32);
 
 train(data, 100, Float32(1.5), Float32(0.01))
 ##
