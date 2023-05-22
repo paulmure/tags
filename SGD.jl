89315ecc1fcaf00afe5e0b39bb9a3fc8a76452de
Paul Mure <paulmure2000@gmail.com>
2023-05-22 03:02:42 -0700

    manual gradient

--- SGD.jl
+++ SGD.jl
@@ -6,57 +6,118 @@ using Plots
 include("./DataLoader.jl")
 using .DataLoader
 
-##
 
-function train(data::MatrixData{Float32}, nfeatures::Int, μ::Float32, learning_rate::Float32)
-    function loss(L, R)
-        pred(u, v) = L[u, :] ⋅ R[:, v]
-        diff_sq(u, v, z) = (pred(u, v) - z)^2
-        l_reg(u) = (μ * sum(L[u, :] .^ 2)) / (2 * data.nnz.row[u])
-        r_reg(v) = (μ * sum(R[:, v] .^ 2)) / (2 * data.nnz.col[v])
-        entry_loss(u, v, z) = diff_sq(u, v, z) + l_reg(u) + r_reg(v)
 
-        sum(entry_loss(u, v, z) for (u, v, z) in zip(findnz(data.data)...))
+struct Params
+    x::Matrix{Float32}
+    y::Matrix{Float32}
+    xb::Vector{Float32}
+    yb::Vector{Float32}
+    μ::Float32
+    λxf::Float32
+    λyf::Float32
+    λxb::Float32
+    λyb::Float32
+    η::Float32
+end
+
+pred(x, y, xb, yb, μ) = (x ⋅ y) + xb + yb + μ
+error(r, x, y, xb, yb, μ) = r - pred(x, y, xb, yb, μ)
+vec_regu(x, λ, nnz) = λ * sum(x .^ 2) / nnz
+
+function sample_loss(r::MatrixData{Float32}, p::Params, u::Int, i::Int)::Float32
+    nnzrow = r.nnz.row[u]
+    nnzcol = r.nnz.col[i]
+    e = error(r.data[u, i], p.x[u, :], p.y[:, i], p.xb[u], p.yb[i], p.μ)
+    x_regu = vec_regu(p.x[u, :], p.λxf, nnzrow)
+    y_regu = vec_regu(p.y[:, i], p.λyf, nnzcol)
+    xb_regu = p.λxb * p.xb[u] / nnzrow
+    yb_regu = p.λyb * p.yb[i] / nnzcol
+    e^2 + x_regu + y_regu + xb_regu + yb_regu
+end
+
+function batch_loss(r::MatrixData{Float32}, p::Params)::Float32
+    sum(sample_loss(r, p, u, i) for (u, i, _) in zip(findnz(r.data)...))
+end
+
+function process_sample!(r::MatrixData{Float32}, p::Params, u::Int, i::Int)::Float32
+    # forward prop
+    nnzrow = r.nnz.row[u]
+    nnzcol = r.nnz.col[i]
+    # There is numerical instability,
+    # y might become NaN if nfeatures is too big
+    # println("($(u), $(i)), y = $(sum(p.y[:, i])), yb = $(p.yb[i])")
+    e = error(r.data[u, i], p.x[u, :], p.y[:, i], p.xb[u], p.yb[i], p.μ)
+    x_regu = vec_regu(p.x[u, :], p.λxf, nnzrow)
+    y_regu = vec_regu(p.y[:, i], p.λyf, nnzcol)
+    xb_regu = p.λxb * p.xb[u] / nnzrow
+    yb_regu = p.λyb * p.yb[i] / nnzcol
+    partial_loss = e^2 + x_regu + y_regu + xb_regu + yb_regu
+    # backward prop
+    p.xb[u] += p.η * (e - xb_regu)
+    p.yb[i] += p.η * (e - yb_regu)
+    p.x[u, :] += p.η .* (e .* p.y[:, i] - x_regu .* p.x[u, :])
+    p.y[:, i] += p.η .* (e .* p.x[u, :] - y_regu .* p.y[:, i])
+    # return
+    partial_loss
+end
+
+function process_batch!(r::MatrixData{Float32}, p::Params)::Float32
+    loss = 0
+    for (u, i, _) in zip(findnz(r.data)...)
+        loss += process_sample!(r, p, u, i)
     end
+    loss
+end
 
-    gt = ReverseDiff.GradientTape(loss, (rand(data.nrows, nfeatures), rand(nfeatures, data.ncols)))
-    backprop = ReverseDiff.compile(gt)
-
-    L = rand(data.nrows, nfeatures)
-    R = rand(nfeatures, data.ncols)
-
-    inputs = (L, R)
-    results = (similar(L), similar(R))
-
+function train(r::MatrixData{Float32}, p::Params)
     history = []
-    initial_loss = loss(L, R)
+    initial_loss = batch_loss(r, p)
     println(initial_loss)
     push!(history, initial_loss)
-
-    for i in 1:100
-        ReverseDiff.gradient!(results, backprop, inputs)
-        L -= results[1] .* learning_rate
-        R -= results[2] .* learning_rate
-
-        curr_loss = loss(L, R)
+    for i in 1:1000
+        curr_loss = process_batch!(r, p)
         last_loss = history[end]
-
         push!(history, curr_loss)
-
         println(curr_loss)
-
         if (i > 100) && (curr_loss > last_loss)
             break
         end
     end
-
     plot(1:length(history), history, label="loss")
     xlabel!("Iteration")
     ylabel!("Loss")
 end
 
-data = load_netflix_data(32);
+function new_params(
+    r::MatrixData{Float32},
+    nfeatures::Int,
+    μ::Float32,
+    λxf::Float32,
+    λyf::Float32,
+    λxb::Float32,
+    λyb::Float32,
+    η::Float32)::Params
+    x = randn(Float32, (r.nrows, nfeatures))
+    y = randn(Float32, (nfeatures, r.ncols))
+    xb = randn(Float32, (r.nrows))
+    yb = randn(Float32, (r.ncols))
+    Params(x, y, xb, yb, μ, λxf, λyf, λxb, λyb, η)
+end
 
-train(data, 100, Float32(1.5), Float32(0.01))
-##
+function run()
+    r = load_netflix_data(128)
+    p = new_params(
+        r,
+        20, # nfeatures
+        convert(Float32, 1), # μ
+        convert(Float32, 1), # λxf
+        convert(Float32, 1), # λyf
+        convert(Float32, 1), # λxb
+        convert(Float32, 1), # λyb
+        convert(Float32, 0.01), # η
+    )
+    train(r, p)
+end
 
+run()
