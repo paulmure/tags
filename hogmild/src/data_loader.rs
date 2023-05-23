88789458a9e6c2b67237012f194554ef5c5eea05
Paul Mure <paulmure2000@gmail.com>
2023-05-22 23:27:21 -0700

    data loader

--- hogmild/src/data_loader.rs
+++ hogmild/src/data_loader.rs
@@ -1,8 +1,9 @@
+use indicatif::ProgressBar;
 use once_cell::sync::Lazy;
-use polars::lazy::dsl::col;
 use polars::prelude::*;
+use rayon::prelude::*;
 use std::collections::HashMap;
-use std::fs::{self, ReadDir};
+use std::fs::{read_dir, DirEntry};
 use std::path::{Path, PathBuf};
 
 static NETFLIX_FILE_SCHEMA: Lazy<SchemaRef> = Lazy::new(|| {
@@ -30,8 +31,8 @@ fn get_movie_id(path: &Path) -> u32 {
 }
 
 #[allow(unused_must_use)]
-fn load_one_movie(paths: &mut ReadDir) -> DataFrame {
-    let path = paths.next().unwrap().unwrap().path();
+fn load_one_movie(dir_entry: DirEntry) -> DataFrame {
+    let path = dir_entry.path();
     let id = get_movie_id(&path);
 
     let mut df = CsvReader::from_path(path)
@@ -49,42 +50,52 @@ fn load_one_movie(paths: &mut ReadDir) -> DataFrame {
     df
 }
 
-/// Reset a field to be indices from 0 to n
-fn tidy_indices(df: &mut DataFrame, field: &str) {
-    let unique_ids = df.column(field).unwrap().unique().unwrap();
+fn remap_indices(data: &Series) -> Series {
+    let unique_ids = data.unique().unwrap();
     let unique_ids_int = unique_ids.u32().unwrap();
 
-    let mut unique_ids_dict: HashMap<u32, u32> = HashMap::new();
+    let mut id_lookup: HashMap<u32, u32> = HashMap::new();
     for (i, val) in unique_ids_int.into_iter().enumerate() {
-        unique_ids_dict.insert(val.unwrap(), i as u32);
+        id_lookup.insert(val.unwrap(), i as u32);
     }
 
-    df.apply(field, |id_series| {
-        id_series
-            .u32()
-            .unwrap()
-            .into_iter()
-            .map(|opt_id| opt_id.map(|id| unique_ids_dict[&id]))
-            .collect::<UInt32Chunked>()
-            .into_series()
-    })
-    .unwrap();
+    let chunked_array = data.u32().unwrap();
+    let vec_option_u32: Vec<Option<u32>> = chunked_array.into_iter().collect();
+
+    vec_option_u32
+        .into_par_iter()
+        .map(|opt_id| opt_id.map(|id| id_lookup[&id]))
+        .collect::<UInt32Chunked>()
+        .into_series()
+}
+
+/// Reset a field to be indices from 0 to n
+fn tidy_indices(df: &mut DataFrame, field: &str) {
+    df.apply(field, remap_indices).unwrap();
 }
 
 pub fn get_netflix_data(n: usize) -> DataFrame {
     assert_ne!(n, 0, "Must load at least one movie");
 
-    let mut paths = fs::read_dir(get_data_dir()).unwrap();
-    let mut res = load_one_movie(&mut paths);
-    let join_ons = ["User", "Movie", "Rating"];
+    println!("Loading netflix data...");
+    let bar = ProgressBar::new(n as u64);
+    bar.inc(1);
 
+    let mut paths = read_dir(get_data_dir()).unwrap();
+    let mut next_dir_entry = || paths.next().unwrap().unwrap();
+
+    let mut res: DataFrame = load_one_movie(next_dir_entry());
+
+    let join_ons = ["User", "Movie", "Rating"];
     for _ in 1..n {
-        let df = load_one_movie(&mut paths);
+        bar.inc(1);
+        let df = load_one_movie(next_dir_entry());
         res = res.outer_join(&df, join_ons, join_ons).unwrap();
     }
 
     tidy_indices(&mut res, "User");
     tidy_indices(&mut res, "Movie");
+    bar.finish();
 
     res
 }
