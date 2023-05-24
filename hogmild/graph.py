ee52e7597ee610e358c72075d35099199024d581
Paul Mure <paulmure2000@gmail.com>
2023-05-24 04:16:15 -0700

    plotting

--- hogmild/graph.py
+++ hogmild/graph.py
@@ -2,15 +2,24 @@
 
 import sys
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 file = sys.argv[1]
 
 with open(file, "r") as f:
     lines = f.readlines()
 history = list(map(lambda x: float(x), lines))
+xs = list(range(len(history)))
 
-plt.xlabel("Epoch")
-plt.ylabel("Loss")
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 8), dpi=400)
+f.suptitle("Netflix Matrix Completion")
 
-plt.plot(range(len(history)), history)
-plt.savefig("loss.png")
+ax1.set_xlabel("Epoch")
+ax1.set_ylabel("Loss")
+ax2.set_xlabel("Epoch")
+ax2.set_ylabel("Loss")
+
+ax1.plot(xs, history)
+ax2.loglog(xs, history)
+
+plt.savefig(f"{Path(file).stem}.png")
