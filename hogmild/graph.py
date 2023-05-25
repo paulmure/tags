f2bee07e66be72dc69d13c1fbb2f332cd648a6b5
Paul Mure <paulmure2000@gmail.com>
2023-05-25 15:17:35 -0700

    update graphing

--- hogmild/graph.py
+++ hogmild/graph.py
@@ -3,23 +3,48 @@
 import sys
 import matplotlib.pyplot as plt
 from pathlib import Path
+from collections import namedtuple
 
-file = sys.argv[1]
+History = namedtuple("History", "name xs history")
 
-with open(file, "r") as f:
-    lines = f.readlines()
-history = list(map(lambda x: float(x), lines))
-xs = list(range(len(history)))
 
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 8), dpi=400)
-f.suptitle("Netflix Matrix Completion")
+def load_history(file: str) -> History:
+    with open(file, "r") as f:
+        lines = f.readlines()
 
-ax1.set_xlabel("Epoch")
-ax1.set_ylabel("Loss")
-ax2.set_xlabel("Epoch")
-ax2.set_ylabel("Loss")
+    name = Path(file).stem
+    history = list(map(lambda x: float(x.split(",")[0]), lines))
+    xs = list(range(len(history)))
 
-ax1.plot(xs, history)
-ax2.loglog(xs, history)
+    return History(name, xs, history)
 
-plt.savefig(f"{Path(file).stem}.png")
+
+def plot_histories(hists: list[History]):
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 8), dpi=400)
+    f.suptitle("Netflix Matrix Completion")
+
+    ax1.set_xlabel("Epoch")
+    ax1.set_ylabel("Loss")
+
+    list(map(lambda h: ax1.plot(h.xs, h.history, label=h.name), hists))
+    ax1.legend(loc="upper right")
+
+    ax2.set_xlabel("Epoch")
+    ax2.set_ylabel("Loss")
+
+    list(map(lambda h: ax2.loglog(h.xs, h.history, label=h.name), hists))
+    ax2.legend(loc="upper right")
+
+    names = list(map(lambda h: h.name, hists))
+    output_file = f"{'_'.join(names)}.png"
+    plt.savefig(output_file)
+
+
+def main():
+    files = sys.argv[1:]
+    histories = list(map(load_history, files))
+    plot_histories(histories)
+
+
+if __name__ == "__main__":
+    main()
