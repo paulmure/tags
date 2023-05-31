ea0edbe96bf39419967b451063c90ea1e4ad650a
Paul Mure <paulmure2000@gmail.com>
2023-05-31 15:23:34 -0700

    max

--- hogmild/src/main.rs
+++ hogmild/src/main.rs
@@ -16,7 +16,9 @@ static ARGS: Lazy<Args> = Lazy::new(|| Args::parse());
 
 fn main() {
     let logs = run_simulation(&ARGS, ARGS.num_samples);
-    let max_time = logs.iter().map(|s| s.time).max().unwrap();
+    // logs.into_iter()
+    //     .for_each(|s| println!("{}, {}", s.time, s.weight_version));
+    let max_time = logs.into_iter().map(|s| s.time).max().unwrap();
     println!("{}", max_time);
     // match args.model.as_str() {
     //     "mat_comp" => matrix_completion::run(args),
